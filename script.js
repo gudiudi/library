11966403e57b962d8ce3cb419054e9b89ae431dd
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-13 16:50:04 +0700

    feat: add basic render functionality with static data

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let library = [];
 
-function Book(title, author, year, pages, isRead) {
+function Book(title, author, pages, isRead) {
   if (!new.target) throw Error("You must use the 'new' operator to call the constructor");
 
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
-  this.year = year;
   this.pages = pages;
   this.isRead = isRead;
 }
@@ -15,13 +14,10 @@ Book.prototype.toggleReadStatus = function() {
   this.isRead = !this.isRead;
 }
 
-function addBookToLibrary(title, author, year, pages, isRead) {
-  const book = new Book(title, author, year, pages, isRead);
+function addBookToLibrary(title, author, pages, isRead) {
+  const book = new Book(title, author, pages, isRead);
   library.push(book);
 }
-
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 1960, 281, true);
-addBookToLibrary("1984", "George Orwell", 1949, 328, false);
 
 function toggleBookFromLibrary(id) {
   const book = library.find(book => book.id === id);
@@ -29,15 +25,51 @@ function toggleBookFromLibrary(id) {
   book.toggleReadStatus();
 }
 
-toggleBookFromLibrary(library[0].id);
-console.log(library);
-
-
 function removeBookFromLibrary(id) {
   const initialLength = library.length;
   library = library.filter(book => book.id !== id);
   if (library.length === initialLength) throw Error("Invalid book id!");
 }
 
-removeBookFromLibrary(library[0].id);
-console.log(library);
+function createBookElement(book) {
+  const container = document.querySelector(".container");
+
+  const div = document.createElement("div");
+  div.classList.add("book");
+  
+  const bookTitle = document.createElement("div");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = book.title;
+
+  const bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = book.author;
+
+  const bookPages = document.createElement("div");
+  bookPages.classList.add("book-pages");
+  bookPages.textContent = book.pages;
+
+  const bookReadStatus = document.createElement("div");
+  bookReadStatus.classList.add("book-read-status");
+  bookReadStatus.textContent = book.isRead ? "Read" : "Not read";
+  
+  container.appendChild(div);
+  div.appendChild(bookTitle);
+  div.appendChild(bookAuthor);
+  div.appendChild(bookPages);
+  div.appendChild(bookReadStatus);
+}
+
+(() => {
+  const addBookButton = document.querySelector("button");
+  
+  addBookButton.addEventListener("click", (e) => {
+    const title = "To Kill a Mockingbird";
+    const author = "Harper Lee";
+    const pages = 281;
+    const isRead = true;
+
+    addBookToLibrary(title, author, pages, isRead);
+    createBookElement(library[library.length - 1]);
+  });
+})();
